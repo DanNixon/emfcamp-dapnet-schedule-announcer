db06ba422860627b9a9ecac37fba196e03dd8cff
Dan Nixon <dan@dan-nixon.com>
2024-05-24 18:20:07 +0100

    Tidy venue name handling

--- src/event_news.rs
+++ src/event_news.rs
@@ -8,12 +8,14 @@ pub(crate) trait EventExt {
 
 impl EventExt for Event {
     fn to_rubric_news(&self) -> Option<OutgoingNews> {
-        let venue = shorten_venue_name(&self.venue);
-        let msg = format!("{venue}: {}", self.title);
+        let venue = Venue::from_schedule_name(&self.venue);
+
+        let news_number = news_number_for_venue(&venue);
+        let msg = format!("{}: {}", venue_short_name(venue), self.title);
 
         match OutgoingNewsBuilder::default()
             .rubric("emfcamp".to_string())
-            .number(news_number_for_venue(&self.venue))
+            .number(news_number)
             .text(msg)
             .build()
         {
@@ -26,46 +28,75 @@ impl EventExt for Event {
     }
 }
 
-mod venues {
-    // TODO: check that this is actually how they appear in the schedule
-    pub(crate) const STAGE_A: &str = "Stage A";
-    pub(crate) const STAGE_B: &str = "Stage B";
-    pub(crate) const STAGE_C: &str = "Stage C";
-    pub(crate) const WORKSHOP_1: &str = "Workshop 1 (NottingHack)";
-    pub(crate) const WORKSHOP_2: &str = "Workshop 2";
-    pub(crate) const WORKSHOP_3: &str = "Workshop 3 (Furry High Commission)";
-    pub(crate) const WORKSHOP_4: &str = "Workshop 4";
-    pub(crate) const WORKSHOP_5: &str = "Workshop 5";
-    pub(crate) const NULL_SECTOR: &str = "Null Sector";
+enum Venue {
+    StageA,
+    StageB,
+    StageC,
+    Workshop0,
+    Workshop1,
+    Workshop2,
+    Workshop3,
+    Workshop4,
+    Workshop5,
+    Workshop6,
+    YouthWorkshop,
+    NullSector,
+    Other(String),
 }
 
-fn news_number_for_venue(venue: &str) -> i8 {
+impl Venue {
+    fn from_schedule_name(v: &str) -> Self {
+        match v {
+            "Stage A" => Self::StageA,
+            "Stage B" => Self::StageB,
+            "Stage C" => Self::StageC,
+            "Workshop 0 (Drop-in)" => Self::Workshop0,
+            "Workshop 1 (NottingHack)" => Self::Workshop1,
+            "Workshop 2 (Milliways)" => Self::Workshop2,
+            "Workshop 3 (Furry High Commission)" => Self::Workshop3,
+            "Workshop 4 (FieldFX)" => Self::Workshop4,
+            "Workshop 5 (Maths)" => Self::Workshop5,
+            "Workshop 6 (Hardware Hacking)" => Self::Workshop6,
+            "Youth Workshop" => Self::YouthWorkshop,
+            "Null Sector" => Self::NullSector,
+            other => Self::Other(other.to_string()),
+        }
+    }
+}
+
+fn news_number_for_venue(venue: &Venue) -> i8 {
     // This can be between 1 and 10
     match venue {
-        venues::STAGE_A => 1,
-        venues::STAGE_B => 2,
-        venues::STAGE_C => 3,
-        venues::WORKSHOP_1 => 4,
-        venues::WORKSHOP_2 => 5,
-        venues::WORKSHOP_3 => 6,
-        venues::WORKSHOP_4 => 7,
-        venues::WORKSHOP_5 => 8,
-        venues::NULL_SECTOR => 9,
+        Venue::StageA => 1,
+        Venue::StageB => 2,
+        Venue::StageC => 3,
+        Venue::Workshop0 => 4,
+        Venue::Workshop1 => 4,
+        Venue::Workshop2 => 4,
+        Venue::Workshop3 => 4,
+        Venue::Workshop4 => 4,
+        Venue::Workshop5 => 4,
+        Venue::Workshop6 => 4,
+        Venue::YouthWorkshop => 5,
+        Venue::NullSector => 6,
         _ => 10,
     }
 }
 
-fn shorten_venue_name(venue: &str) -> &str {
+fn venue_short_name(venue: Venue) -> String {
     match venue {
-        venues::STAGE_A => "Stg A",
-        venues::STAGE_B => "Stg B",
-        venues::STAGE_C => "Stg C",
-        venues::WORKSHOP_1 => "Wksp 1",
-        venues::WORKSHOP_2 => "Wksp 2",
-        venues::WORKSHOP_3 => "Wksp 3",
-        venues::WORKSHOP_4 => "Wksp 4",
-        venues::WORKSHOP_5 => "Wksp 5",
-        venues::NULL_SECTOR => "Null Sec",
-        _ => venue,
+        Venue::StageA => "Stg A".to_string(),
+        Venue::StageB => "Stg B".to_string(),
+        Venue::StageC => "Stg C".to_string(),
+        Venue::Workshop0 => "Wksp 0".to_string(),
+        Venue::Workshop1 => "Wksp 1".to_string(),
+        Venue::Workshop2 => "Wksp 2".to_string(),
+        Venue::Workshop3 => "Wksp 3".to_string(),
+        Venue::Workshop4 => "Wksp 4".to_string(),
+        Venue::Workshop5 => "Wksp 5".to_string(),
+        Venue::Workshop6 => "Wksp 6".to_string(),
+        Venue::YouthWorkshop => "Yth Wksp".to_string(),
+        Venue::NullSector => "Nul Sec".to_string(),
+        Venue::Other(name) => name,
     }
 }
